8d955b9caf6c13d6e22f063401155c60916042c5
afiqmehdizada <afig.matrix@gmail.com>
2025-10-22 19:42:27 +0400

    Add project files.

---
+++ Hyperspace/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hyperspace.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hyperspace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hyperspace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
